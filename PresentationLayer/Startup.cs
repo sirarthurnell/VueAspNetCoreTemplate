363c039f673d7c1d5de08642226c084314f8992a
Arturo Medina <sirarthurnell@gmail.com>
2019-03-11 15:24:11 +0100

    Tasks added.

--- PresentationLayer/Startup.cs
+++ PresentationLayer/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using VueCliMiddleware;
 
 namespace PresentationLayer
 {
